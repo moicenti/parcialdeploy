2b478fcf4ef7e85b9e2b561106d4a447ff4bd58b
moicenti <128745446+moicenti@users.noreply.github.com>
2024-09-24 14:53:39 -0600

    added parcial1

---
+++ JS/shoping.js
@@ -0,0 +1,21 @@
+
+
+const asignarAccion = () =>{
+
+    const botones = document.querySelectorAll('button');
+    
+    botones.forEach(Element => {
+    
+        Element.addEventListener('click', ()=>{
+
+            alert("Muchas gracias, su compra será procesada en un momento")
+    
+    
+    
+        });
+        
+    });
+    
+    }
+
+    asignarAccion();
